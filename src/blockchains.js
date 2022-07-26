5ab52758a496562cf7d3a3b8721e4107ffea18cc
olifur <olifur@domain.net>
2022-07-26 11:19:16 +0200

    adding avax

--- src/blockchains.js
+++ src/blockchains.js
@@ -1,5 +1,5 @@
-let supported = ['ethereum', 'bsc', 'polygon', 'solana']
-supported.evm = ['ethereum', 'bsc', 'polygon']
+let supported = ['ethereum', 'bsc', 'polygon', 'solana', 'avax', 'avax_testnet']
+supported.evm = ['ethereum', 'bsc', 'polygon', 'avax', 'avax_testnet']
 supported.solana = ['solana']
 
 export { supported }
